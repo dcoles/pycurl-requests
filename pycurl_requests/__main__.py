b805ef8964470208bbd2392f70b25cd39b405533
David Coles <coles.david@gmail.com>
2020-02-01 17:44:48 -0800

    main: Use `run_py` for executing script

--- pycurl_requests/__main__.py
+++ pycurl_requests/__main__.py
@@ -1,6 +1,6 @@
 """Shim for running existing Requests-based script using PycURL-Requests."""
-
 import argparse
+import runpy
 import sys
 
 import pycurl_requests
@@ -15,9 +15,10 @@ def main():
     # Override requests
     sys.modules['requests'] = pycurl_requests
 
-    # Execute target script
-    sys.argv = [args.script] + args.arg
-    exec(open(args.script).read(), globals())
+    # Run script
+    sys.argv[1:] = args.arg
+    runpy.run_path(args.script, run_name='__main__')
+    sys.exit()
 
 
 if __name__ == '__main__':
