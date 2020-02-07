fa0eb3f5e0f2189aa54891e242ac97238f98fa5e
David Coles <coles.david@gmail.com>
2020-02-06 19:09:15 -0800

    Allow running module under Requests compatibility mode

--- pycurl_requests/__main__.py
+++ pycurl_requests/__main__.py
@@ -8,8 +8,10 @@ import pycurl_requests
 
 def main():
     parser = argparse.ArgumentParser(description='a Requests-compatible interface for PycURL')
-    parser.add_argument('script', help='Python script to run')
-    parser.add_argument('arg', nargs='*', help='script arguments')
+    parser.add_argument('-m', dest='is_module', action='store_true',
+                        help='run library module as script')
+    parser.add_argument('name', help='script or module run')
+    parser.add_argument('arg', nargs=argparse.REMAINDER, help='script arguments')
     args = parser.parse_args()
 
     # Override requests
@@ -17,7 +19,10 @@ def main():
 
     # Run script
     sys.argv[1:] = args.arg
-    runpy.run_path(args.script, run_name='__main__')
+    if args.is_module:
+        runpy.run_module(args.name, run_name='__main__', alter_sys=True)
+    else:
+        runpy.run_path(args.name, run_name='__main__')
     sys.exit()
 
 
