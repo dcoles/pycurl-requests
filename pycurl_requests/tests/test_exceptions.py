b3a9420cb0c0994000cf71dec2c563555759d984
David Coles <coles.david@gmail.com>
2020-02-08 12:35:39 -0800

    Add tests for exceptions

---
+++ pycurl_requests/tests/test_exceptions.py
@@ -0,0 +1,22 @@
+import pycurl
+import pytest
+
+import pycurl_requests as requests
+
+
+def test_connecterror_refused():
+    with pytest.raises(requests.ConnectionError) as e:
+        requests.get('http://127.0.0.1:0')
+
+    assert isinstance(e.value.__cause__, pycurl.error)
+    assert e.value.curl_code == pycurl.E_COULDNT_CONNECT
+    assert 'Connection refused' in e.value.curl_message
+
+
+def test_connecterror_resolve():
+    with pytest.raises(requests.ConnectionError) as e:
+        requests.get('http://nosuchdomain.example.com')
+
+    assert isinstance(e.value.__cause__, pycurl.error)
+    assert e.value.curl_code == pycurl.E_COULDNT_RESOLVE_HOST
+    assert 'Could not resolve host' in e.value.curl_message
