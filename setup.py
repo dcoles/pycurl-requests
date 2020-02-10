820e5d7cee3d5055c09e7ad3f9abda96ff633148
David Coles <coles.david@gmail.com>
2020-02-09 22:11:03 -0800

    Bump version to 0.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name='pycurl-requests',
-    version='0.0.4',
+    version='0.1.0',
     description='A Requests-compatible interface for pycURL',
     long_description=long_description,
     long_description_content_type="text/markdown",
